6c7adae843d69dbd6a64944d6e4b68fdcb6bcbf2
= <=>
2022-10-14 14:19:12 +0200

    Avances en clase con Yessi

--- MananadeLocos/app/src/main/java/com/example/maanadelocos/MainActivity.java
+++ MananadeLocos/app/src/main/java/com/example/maanadelocos/MainActivity.java
@@ -1,37 +1,44 @@
 package com.example.maanadelocos;
+import static android.view.View.INVISIBLE;
+
+import android.annotation.SuppressLint;
 import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView cajatexto = (TextView) findViewById(R.id.cajatexto1);
+        TextView cajatexto = findViewById(R.id.cajatexto1);
         TextView dialogo = (TextView) findViewById(R.id.dialogo);
-        ImageView maki = (ImageView) findViewById(R.id.maki);
-        Button boton1 = (Button) findViewById(R.id.boton1);
-        Button boton2 = (Button) findViewById(R.id.boton2);
+        ImageView maki = findViewById(R.id.maki);
+        ImageView pixies = findViewById(R.id.pixies);
+        Button boton1 = findViewById(R.id.boton1);
+        Button boton2 = findViewById(R.id.boton2);
 
+        pixies.setVisibility(INVISIBLE);
         boton2.setOnClickListener(view -> {
-            cajatexto.setText("Maki se acuesta en cama otra vwz y al rato empieza a escuchar unos ruidos extraños quesalen de su armario.");
-            dialogo.setText("¿Qué será ese ruido?¿Debería ver que hay dentro del armario?");
+            cajatexto.setText("Maki se está apunto de dormir, otra vez, cuando escucha unos ruidos extraños procedentes de su armario.");
+            dialogo.setText("- ¿Qué serán esos ruidos? - piensa.\n"+"- ¿Debería ir a echar un vistazo al armario?");
             boton1.setText("Abrir el armario");
-            boton2.setText("No abrir el armario.");
+            boton2.setText("No abrir el armario");
             boton1.setOnClickListener(view1 -> {
-                cajatexto.setText("Maki se levanta y abre el armario para encontrarse con algo que nunca se imaginaría.");
+                cajatexto.setText("Maki, todavía no muy convencida, se levanta y va a revisar su armario, encontrandose, en su interior, algo que nunca se podría haber imaginado.");
+                pixies.setVisibility(view.VISIBLE);
+                dialogo.setText("Ya iba siendo hora de que me abrieras, ¿No crees?");
+
             });
         });
 
         boton1.setOnClickListener( view -> {
-            System.out.println("Maki ha ido a clase");
+           // System.out.println("Maki ha ido a clase");
         });
 
     }
--- MananadeLocos/app/src/main/java/com/example/maanadelocos/slapshscreen.java
+++ MananadeLocos/app/src/main/java/com/example/maanadelocos/slapshscreen.java
@@ -3,7 +3,10 @@ package com.example.maanadelocos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class slapshscreen extends AppCompatActivity {
 
@@ -11,5 +14,17 @@ public class slapshscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slapshscreen);
-    }
+
+        ImageView portada = findViewById(R.id.portada);
+
+        portada.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Cuando se pulse, las acciones que realicen se escriben aquí.
+
+                Intent i = new Intent(slapshscreen.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+            }
 }
